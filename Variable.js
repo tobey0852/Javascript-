e25d1e673a0f1b5b9363b473f29e13f9d9e2d663
tobey0852 <profit0852@gmail.com>
2023-07-07 09:40:22 +0100

    Thursday Assignment

--- Variable.js
+++ Variable.js
@@ -1,6 +1,51 @@
-let num_one = 8
-let num_two = 4;
-let sum_num = num_one + num_two
-console.log(sum_num);
+let prompt = require('prompt-sync')({sigint: true})
+
+// let num_one = 8
+// let num_two = 4;
+// let sum_num = num_one + num_two
+// console.log(sum_num);
+
+// QUESTION 2
+// let first_name = prompt("Enter first name:")
+// let last_name = prompt("Enter last name:")
+
+// console.log(first_name, last_name)
+
+//  QUESTION 3
+
+// let num_one = Number(prompt('Enter number one'))
+// let num_two = Number(prompt('Enter number two'))
+// let sum_num = num_one + num_two;
+
+// console.log(sum_num)
 
 
+// THURSDAY ASSIGNMENT
+//       QUESTION 1
+
+
+// let firstName = prompt("Enter first name:");
+// let lastName = prompt("Enter last name:");
+
+// let userAge = Number(prompt("Age of user: "));
+// let currentYear = Number(prompt("Enter your current year: "));
+// let earthlyAppearance = ("currentYear - userAge");
+
+// console.log( firstName + " " + lastName + ", You are " + userAge + "years old and you came to Earth In the year" + earthlyAppearance + ".");
+
+
+// // QUESTION 2
+
+// let name = prompt("Enter first name:");
+// let age = Number(prompt("Enter age:"));
+// let departmentName = prompt( "Enter department name");
+
+// console.log("Welcome" + name + " " + "You are in " + departmentName + "department"
+// + "," + "you are " + age + "years old and your score is 34.72");
+
+
+// QUESTION 3
+let weight = Number(prompt("Enter weight"))
+let poundToKilo = weight * 45;
+
+console.log("poundToKilo");
